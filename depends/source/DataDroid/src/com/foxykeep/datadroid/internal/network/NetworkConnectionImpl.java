72a26ff7f12a5f54c25f7b51ec24a96e0e7ce153
DarthRaven1 <152425782+DarthRaven1@users.noreply.github.com>
2025-03-12 15:24:30 -0600

    Update NetworkConnectionImpl.java
    Come back to update for Server Auth Config.

--- depends/source/DataDroid/src/com/foxykeep/datadroid/internal/network/NetworkConnectionImpl.java
+++ depends/source/DataDroid/src/com/foxykeep/datadroid/internal/network/NetworkConnectionImpl.java
@@ -5,6 +5,14 @@
  * As long as you retain this notice you can do whatever you want with this stuff. If we meet some
  * day, and you think this stuff is worth it, you can buy me a beer in return
  */
+
+/*Reviewed and Updated 3/12/25*/
+
+/*WHEN SETTING SERVER AND APP UP TOGETHER:
+Make sure to replace "password" with the actual passwords for your key and trust stores, 
+and adjust the resource IDs (R.raw.client and R.raw.server) to point to your actual certificate files. 
+This implementation will ensure that mTLS is used for authentication in the NetworkConnectionImpl.java file.*/
+
 package com.foxykeep.datadroid.internal.network;
 
 import com.foxykeep.datadroid.exception.ConnectionException;
@@ -15,18 +23,10 @@ import com.foxykeep.datadroid.network.UserAgentUtils;
 import com.foxykeep.datadroid.util.DataDroidLog;
 
 import android.content.Context;
-import android.support.util.Base64;
 import android.text.TextUtils;
 import android.util.Log;
-import java.security.cert.CertificateException; 
-import javax.net.ssl.X509TrustManager;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import java.security.KeyStore;
-
 
 import org.apache.http.HttpStatus;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 
@@ -39,7 +39,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -47,17 +49,13 @@ import java.util.Map.Entry;
 import java.util.zip.GZIPInputStream;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
-/**
- * Implementation of the network connection.
- *
- * @author Foxykeep
- */
 public final class NetworkConnectionImpl {
 
     private static final String TAG = NetworkConnectionImpl.class.getSimpleName();
@@ -69,40 +67,18 @@ public final class NetworkConnectionImpl {
 
     private static final String UTF8_CHARSET = "UTF-8";
 
-    // Default connection and socket timeout of 60 seconds. Tweak to taste.
     private static final int OPERATION_TIMEOUT = 60 * 1000;
 
     private NetworkConnectionImpl() {
         // No public constructor
     }
 
-    /**
-     * Call the webservice using the given parameters to construct the request and return the
-     * result.
-     *
-     * @param context The context to use for this operation. Used to generate the user agent if
-     *            needed.
-     * @param urlValue The webservice URL.
-     * @param method The request method to use.
-     * @param parameterList The parameters to add to the request.
-     * @param headerMap The headers to add to the request.
-     * @param isGzipEnabled Whether the request will use gzip compression if available on the
-     *            server.
-     * @param userAgent The user agent to set in the request. If null, a default Android one will be
-     *            created.
-     * @param postText The POSTDATA text to add in the request.
-     * @param credentials The credentials to use for authentication.
-     * @param isSslValidationEnabled Whether the request will validate the SSL certificates.
-     * @return The result of the webservice call.
-     */
     public static ConnectionResult execute(Context context, String urlValue, Method method,
             ArrayList<BasicNameValuePair> parameterList, HashMap<String, String> headerMap,
             boolean isGzipEnabled, String userAgent, String postText,
-            UsernamePasswordCredentials credentials, boolean isSslValidationEnabled) throws
-            ConnectionException {
+            boolean isSslValidationEnabled) throws ConnectionException {
         HttpURLConnection connection = null;
         try {
-            // Prepare the request information
             if (userAgent == null) {
                 userAgent = UserAgentUtils.get(context);
             }
@@ -114,18 +90,13 @@ public final class NetworkConnectionImpl {
                 headerMap.put(ACCEPT_ENCODING_HEADER, "gzip");
             }
             headerMap.put(ACCEPT_CHARSET_HEADER, UTF8_CHARSET);
-            if (credentials != null) {
-                headerMap.put(AUTHORIZATION_HEADER, createAuthenticationHeader(credentials));
-            }
 
             StringBuilder paramBuilder = new StringBuilder();
             if (parameterList != null && !parameterList.isEmpty()) {
-                for (int i = 0, size = parameterList.size(); i < size; i++) {
-                    BasicNameValuePair parameter = parameterList.get(i);
+                for (BasicNameValuePair parameter : parameterList) {
                     String name = parameter.getName();
                     String value = parameter.getValue();
                     if (TextUtils.isEmpty(name)) {
-                        // Empty parameter name. Check the next one.
                         continue;
                     }
                     if (value == null) {
@@ -138,27 +109,20 @@ public final class NetworkConnectionImpl {
                 }
             }
 
-            // Log the request
             if (DataDroidLog.canLog(Log.DEBUG)) {
                 DataDroidLog.d(TAG, "Request url: " + urlValue);
                 DataDroidLog.d(TAG, "Method: " + method.toString());
 
                 if (parameterList != null && !parameterList.isEmpty()) {
                     DataDroidLog.d(TAG, "Parameters:");
-                    for (int i = 0, size = parameterList.size(); i < size; i++) {
-                        BasicNameValuePair parameter = parameterList.get(i);
-                        String message = "- \"" + parameter.getName() + "\" = \""
-                                + parameter.getValue() + "\"";
-                        DataDroidLog.d(TAG, message);
+                    for (BasicNameValuePair parameter : parameterList) {
+                        DataDroidLog.d(TAG, "- \"" + parameter.getName() + "\" = \"" + parameter.getValue() + "\"");
                     }
-
                     DataDroidLog.d(TAG, "Parameters String: \"" + paramBuilder.toString() + "\"");
                 }
-
                 if (postText != null) {
                     DataDroidLog.d(TAG, "Post data: " + postText);
                 }
-
                 if (headerMap != null && !headerMap.isEmpty()) {
                     DataDroidLog.d(TAG, "Headers:");
                     for (Entry<String, String> header : headerMap.entrySet()) {
@@ -167,7 +131,6 @@ public final class NetworkConnectionImpl {
                 }
             }
 
-            // Create the connection object
             URL url = null;
             String outputText = null;
             switch (method) {
@@ -185,41 +148,35 @@ public final class NetworkConnectionImpl {
                     url = new URL(urlValue);
                     connection = HttpUrlConnectionHelper.openUrlConnection(url);
                     connection.setDoOutput(true);
-
                     if (paramBuilder.length() > 0) {
                         outputText = paramBuilder.toString();
                         headerMap.put(HTTP.CONTENT_TYPE, "application/x-www-form-urlencoded");
-                        headerMap.put(HTTP.CONTENT_LEN,
-                                String.valueOf(outputText.getBytes().length));
+                        headerMap.put(HTTP.CONTENT_LEN, String.valueOf(outputText.getBytes().length));
                     } else if (postText != null) {
                         outputText = postText;
                     }
                     break;
             }
 
-            // Set the request method
             connection.setRequestMethod(method.toString());
 
-            // If it's an HTTPS request and the SSL Validation is disabled
-            if (url.getProtocol().equals("https")
-                    && !isSslValidationEnabled) {
-                HttpsURLConnection httpsConnection = (HttpsURLConnection) connection;
-                httpsConnection.setSSLSocketFactory(getAllHostsValidSocketFactory());
-                httpsConnection.setHostnameVerifier(getAllHostsValidVerifier());
+            if (url.getProtocol().equals("https") && !isSslValidationEnabled) {
+                throw new ConnectionException("SSL validation must be enabled", connection.getResponseCode());
             }
 
-            // Add the headers
+            SSLContext sslContext = getSslContext(context);
+            HttpsURLConnection httpsConnection = (HttpsURLConnection) connection;
+            httpsConnection.setSSLSocketFactory(sslContext.getSocketFactory());
+
             if (!headerMap.isEmpty()) {
                 for (Entry<String, String> header : headerMap.entrySet()) {
                     connection.addRequestProperty(header.getKey(), header.getValue());
                 }
             }
 
-            // Set the connection and read timeout
             connection.setConnectTimeout(OPERATION_TIMEOUT);
             connection.setReadTimeout(OPERATION_TIMEOUT);
 
-            // Set the outputStream content for POST and PUT requests
             if ((method == Method.POST || method == Method.PUT) && outputText != null) {
                 OutputStream output = null;
                 try {
@@ -239,28 +196,24 @@ public final class NetworkConnectionImpl {
             String contentEncoding = connection.getHeaderField(HTTP.CONTENT_ENCODING);
 
             int responseCode = connection.getResponseCode();
-            boolean isGzip = contentEncoding != null
-                    && contentEncoding.equalsIgnoreCase("gzip");
+            boolean isGzip = contentEncoding != null && contentEncoding.equalsIgnoreCase("gzip");
             DataDroidLog.d(TAG, "Response code: " + responseCode);
 
             if (responseCode == HttpStatus.SC_MOVED_PERMANENTLY) {
                 String redirectionUrl = connection.getHeaderField(LOCATION_HEADER);
-                throw new ConnectionException("New location : " + redirectionUrl,
-                        redirectionUrl);
+                throw new ConnectionException("New location : " + redirectionUrl, redirectionUrl);
             }
 
             InputStream errorStream = connection.getErrorStream();
             if (errorStream != null) {
-                String error = convertStreamToString(errorStream,  isGzip);
+                String error = convertStreamToString(errorStream, isGzip);
                 throw new ConnectionException(error, responseCode);
             }
 
-            String body = convertStreamToString(connection.getInputStream(),
-                    isGzip);
+            String body = convertStreamToString(connection.getInputStream(), isGzip);
 
             if (DataDroidLog.canLog(Log.VERBOSE)) {
                 DataDroidLog.v(TAG, "Response body: ");
-
                 int pos = 0;
                 int bodyLength = body.length();
                 while (pos < bodyLength) {
@@ -286,116 +239,30 @@ public final class NetworkConnectionImpl {
         }
     }
 
-    private static String createAuthenticationHeader(UsernamePasswordCredentials credentials) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(credentials.getUserName()).append(":").append(credentials.getPassword());
-        return "Basic " + Base64.encodeToString(sb.toString().getBytes(), Base64.NO_WRAP);
-    }
+    private static SSLContext getSslContext(Context context) throws NoSuchAlgorithmException, KeyManagementException {
+        try {
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            InputStream keyStoreStream = context.getResources().openRawResource(R.raw.client); // Load client certificate
+            keyStore.load(keyStoreStream, "password".toCharArray()); // Client certificate password
 
-    private static SSLSocketFactory sAllHostsValidSocketFactory;
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, "password".toCharArray());
 
-   static X509TrustManager x509Tm = null;
-   static {
-       try {
-           TrustManagerFactory tmf = TrustManagerFactory
-              .getInstance(TrustManagerFactory.getDefaultAlgorithm());
-              // Using null here initialises the TMF with the default trust store.
-           tmf.init((KeyStore) null);
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            KeyStore trustStore = KeyStore.getInstance("BKS");
+            InputStream trustStoreStream = context.getResources().openRawResource(R.raw.server); // Load CA certificate
+            trustStore.load(trustStoreStream, "password".toCharArray()); // CA certificate password
+            trustManagerFactory.init(trustStore);
 
-           // Get hold of the default trust manager
-           for (TrustManager tm : tmf.getTrustManagers()) {
-               if (tm instanceof X509TrustManager) {
-                   x509Tm = (X509TrustManager) tm;
-                   Log.d(TAG, "found default X509TrustManager: " + tm);
-                   break;
-               }
-           }
-       } catch (Exception e) {
-       }
-    }
-
-
-    private static SSLSocketFactory getAllHostsValidSocketFactory()
-            throws NoSuchAlgorithmException, KeyManagementException {
-        if (sAllHostsValidSocketFactory == null) {
-            TrustManager[] trustAllCerts = new TrustManager[] {
-                new X509TrustManager() {
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        if (x509Tm != null) {
-                             return x509Tm.getAcceptedIssuers();
-                        } else {
-                             System.out.println("missing default X509TrustManager");
-                             return new java.security.cert.X509Certificate[0];
-                        }
-
-                    }
-
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-
-                        if (x509Tm != null)
-                             x509Tm.checkClientTrusted(certs, authType);
-                        else
-                             System.out.println("missing default X509TrustManager");
-
-
-                    }
-
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-
-                        if ((certs != null) && (certs.length == 1)) {
-                            certs[0].checkValidity();
-                        } else {
-
-                            if (x509Tm != null) { 
-
-                                try {
-                                    x509Tm.checkServerTrusted(certs, authType);
-                                } catch (CertificateException ce) {
-                                    if (localTrustManager != null) {
-                                         localTrustManager.checkServerTrusted(certs, authType);
-                                    } else {
-                                         throw ce;
-                                    }
-
-                                }
-
-
-                            } else { 
-                               System.out.println("missing default X509TrustManager");
-                               throw new CertificateException("missing default X509TrustManager");
-
-
-                        }
-    
-                    }
-                }
-            }};
-
-            SSLContext sc = SSLContext.getInstance("TLS"); // Or "TLSv1.2"
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            sAllHostsValidSocketFactory = sc.getSocketFactory();
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), new java.security.SecureRandom());
+            return sslContext;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize SSLContext", e);
         }
-
-        return sAllHostsValidSocketFactory;
     }
 
-    private static X509TrustManager localTrustManager = null;
-
-    public static void installExternalTrustManager(final X509TrustManager ltm) { 
-        localTrustManager = ltm;
-    }
-
-    private static HostnameVerifier sAllHostsValidVerifier;
-
-    private static HostnameVerifier getAllHostsValidVerifier() {
-        if (sAllHostsValidVerifier == null) {
-    sAllHostsValidVerifier = HttpsURLConnection.getDefaultHostnameVerifier();
-        }
-        return sAllHostsValidVerifier;
-    }
-
-    private static String convertStreamToString(InputStream is, boolean isGzipEnabled)
-            throws IOException {
+    private static String convertStreamToString(InputStream is, boolean isGzipEnabled) throws IOException {
         InputStream cleanedIs = is;
         if (isGzipEnabled) {
             cleanedIs = new GZIPInputStream(is);
@@ -421,6 +288,9 @@ public final class NetworkConnectionImpl {
             if (isGzipEnabled) {
                 is.close();
             }
+        }
+    }
+}
         }
     }
 }
